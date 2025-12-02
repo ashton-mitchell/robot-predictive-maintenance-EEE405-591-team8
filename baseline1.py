67ff03df2ce7a589129c8f919e4090cffbc37212
KSRIPADA <ksripada@asu.edu>
2025-12-01 18:20:28 -0700

    Updated independent variables for baseline

--- baseline1.py
+++ baseline1.py
@@ -11,35 +11,41 @@ from sklearn.metrics import mean_squared_error
 import numpy as np
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
-from sklearn.preprocessing import MinMaxScaler
+# from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import log_loss
-
+import matplotlib.pyplot as plt
 df = pd.read_csv("./results/train_snapshot.csv")
-ps = MinMaxScaler()
+# ps = MinMaxScaler()
 df = df.dropna()
-X = df.loc[:,'time (months)':'crack length (arbitary unit)']
-ps.fit(X)
-X = ps.transform(X)
+X = df.loc[:,'crack length (arbitary unit)']
+X_1 = X.values
+X_1 = X_1.reshape(-1, 1)
+X = X_1
+# ps.fit(X)
+# X = ps.transform(X)
 y1 = df.loc[:,'ttf_months'] 
 y2 = df.loc[:,'fail_in_4m']
 
 
-predicted_failure = Ridge(alpha=0.0)
+predicted_failure = Ridge(alpha=0)
 
 predicted_failure.fit(X,y1)
+w = predicted_failure.coef_
 yP = predicted_failure.predict(X)
 mse = mean_squared_error(y1, yP)
 print("Predictions", yP)
 
 print("Error in Predicted Failure Months:", mse)
-
-log = LogisticRegression(class_weight='balanced', solver = 'saga')
+print("Weights: ", w)
+log = LogisticRegression(class_weight='balanced', C=10000)
 log.fit(X,y2)
 y2P = log.predict(X)
 mse2 = log_loss(y2, y2P)
 print("Logistic Regression:", y2P)
 print("Error in Logistic Regression:", mse2)
 
-
-
+plt.plot(X,yP)
+plt.show()
+plt.show()
+plt.plot(yP, y1)
 
